ca3a1350afc57eaabfc4a7471572d5f7f0db0f41
Samuli Ulmanen <samuli.ulmanen@rovio.com>
2014-10-04 15:29:55 +0300

    move directive common functions out of linking function

--- app/js/angular/directive/ecSlider.js
+++ app/js/angular/directive/ecSlider.js
@@ -1,6 +1,76 @@
 angular.module('ecSlider').directive('ecSlider', ['$timeout',
     function($timeout) {
         'use strict';
+
+        function sanitize(newVal) {
+            var i;
+            if (isArray(newVal)) {
+                for (i = 0; i < newVal.length; i++) {
+                    if (typeof newVal[i] === 'string') {
+                        newVal[i] = parseFloat(newVal[i]);
+                    }
+                }
+            } else {
+                if (typeof newVal === 'string') {
+                    newVal = parseFloat(newVal);
+                }
+            }
+
+            return newVal;
+        }
+
+        function isArray(a) {
+            if (Object.prototype.toString.call(a) === '[object Array]') {
+                return true;
+            }
+            return false;
+        }
+        function allInBetween(newVal, config) {
+            var i;
+            for (i = 0; i < newVal.length; i++) {
+                if (!inBetween(newVal[i], config)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function inBetween(newVal, config) {
+            return (newVal >= config.min) &&
+                (newVal <= config.max);
+        }
+
+        function inRange(newVal, config) {
+            return (isArray(newVal) ? allInBetween(newVal, config) : inBetween(newVal, config));
+
+        }
+
+        function isDefined(val) {
+            return (val || val === 0);
+        }
+
+        function init(el, config, ctrl, scope) {
+            var s;
+            if (config &&
+                isDefined(config.min) &&
+                isDefined(config.max) &&
+                isDefined(config.value)) {
+
+                s = el.slider(config);
+                s.on('slide', function(e) {
+                    var newVal = e.value;
+                    if(isDefined(newVal) &&
+                       inRange(newVal, scope.config)) {
+                        ctrl.$setViewValue(newVal);
+                        $timeout(function() {
+                            scope.$digest();
+                        });
+                    }
+                });
+            }
+            return s;
+        }
+
         return {
             require: 'ngModel',
             replace: true,
@@ -11,69 +81,6 @@ angular.module('ecSlider').directive('ecSlider', ['$timeout',
             },
             link: function(scope, el, attrs, ctrl) {
                 var slider,
-                    sanitize = function sanitize(newVal) {
-                        var i;
-                        if (isArray(newVal)) {
-                            for (i = 0; i < newVal.length; i++) {
-                                if (typeof newVal[i] === 'string') {
-                                    newVal[i] = parseFloat(newVal[i]);
-                                }
-                            }
-                        } else {
-                            if (typeof newVal === 'string') {
-                                newVal = parseFloat(newVal);
-                            }
-                        }
-
-                        return newVal;
-                    },
-                    isArray = function isArray(a) {
-                        if (Object.prototype.toString.call(a) === '[object Array]') {
-                            return true;
-                        }
-                        return false;
-                    },
-                    allInBetween = function allInBetween(newVal, config) {
-                        var i;
-                        for (i = 0; i < newVal.length; i++) {
-                            if (!inBetween(newVal[i], config)) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    },
-                    inBetween = function inBetween(newVal, config) {
-                        return (newVal >= config.min) &&
-                            (newVal <= config.max);
-                    },
-                    inRange = function inRange(newVal, config) {
-                        return (isArray(newVal) ? allInBetween(newVal, config) : inBetween(newVal, config));
-
-                    },
-                    isDefined = function isDefined(val) {
-                        return (val || val === 0);
-                    },
-                    init = function init(el, config, ctrl, scope) {
-                        var s;
-                        if (config &&
-                            isDefined(config.min) &&
-                            isDefined(config.max) &&
-                            isDefined(config.value)) {
-
-                            s = el.slider(config);
-                            s.on('slide', function(e) {
-                                var newVal = e.value;
-                                if(isDefined(newVal) &&
-                                   inRange(newVal, scope.config)) {
-                                    ctrl.$setViewValue(newVal);
-                                    $timeout(function() {
-                                        scope.$digest();
-                                    });
-                                }
-                            });
-                        }
-                        return s;
-                    },
                     render = function render(el, config, ctrl) {
                         $timeout(function() {
                             slider = init(el, config, ctrl, scope);
